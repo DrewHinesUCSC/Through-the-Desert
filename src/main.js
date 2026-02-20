edaabc6ce947b0dff434e71687017f9aa1bb4f25
DrewHinesUCSC <drhines@ucsc.edu>
2026-02-20 15:43:03 -0800

    Added Comments Header, Final Commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+/* 
+Drew Hines
+02/20/2026
+~35 hours
+I utilized all the Phaser examples and documentations along with
+a couple of YouTube creators I have found very useful. And of course
+Professor Altice's repository and slides
+ //https://www.youtube.com/watch?v=-GGU2fizDT4
+
+ My creative tilt is that I created the entire asthetic from scratch. I drew
+ the main character myself, and used photos from my album to create and animate
+ the sprites and backgrounds. On the technical side, I used a finite state machine to 
+ handle the punk characters movements, and used gouping with the obstacels to keep track
+ of when one was destroyed and when another needs to be generated. I created the background
+ music using GarageBand and LogicPro.
+
+*/
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
